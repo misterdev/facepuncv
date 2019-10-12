a921335bf525c8ef8bc4b8f9eec5fe53eded4c7d
Devid Farinelli <devid.farinelli@gmail.com>
2019-10-12 14:16:43 +0200

    active item and animations

--- src/Equipped.svelte
+++ src/Equipped.svelte
@@ -4,22 +4,22 @@
 </script>
 
 <div id="wrapper">
-    <a class="item" href="https://www.youtube.com/watch?v=z9Uz1icjwrM">
+    <a class="item" href="https://www.youtube.com/watch?v=z9Uz1icjwrM" target="_blank">
         <img src="images/rock.png" alt="rock"/>
     </a>
-    <a class="item" href="https://github.com/misterdev">
+    <a class="item" href="https://github.com/misterdev" target="_blank">
         <img src="images/github3d.png" alt="github"/>
     </a>
-    <a class="item" href="https://twitter.com/misterdev_">
+    <a class="item" href="https://twitter.com/misterdev_" target="_blank">
         <img src="images/twitter3d2.png" alt="twitter"/>
     </a>
-    <a class="item" href="https://www.linkedin.com/in/misterdev/">
+    <a class="item" href="https://www.linkedin.com/in/misterdev/" target="_blank">
         <img src="images/linkedin3d.png" alt="linkedin"/>
     </a>
-    <a class="item" href="https://stackoverflow.com/users/4695325/devid-farinelli">
+    <a class="item" href="https://stackoverflow.com/users/4695325/devid-farinelli" target="_blank">
         <img src="images/stack3d.png" alt="stackoverflow"/>
     </a>
-    <a class="item" href="mailto:devid.farinelli@gmail.com">
+    <a class="item" href="mailto:devid.farinelli@gmail.com" target="_blank">
         <img src="images/envelope3d.png" alt="e-mail"/>
     </a>
 </div>
--- src/crafting/Categories.svelte
+++ src/crafting/Categories.svelte
@@ -3,7 +3,12 @@
     import MdBusinessCenter from 'svelte-icons/md/MdBusinessCenter.svelte'
     import MdSchool from 'svelte-icons/md/MdSchool.svelte'
     import MdFolder from 'svelte-icons/md/MdFolder.svelte'
-    // const items = Object.keys(categories).map(k => categories[k]) 
+
+    let activeCat = 0;
+    navigation.subscribe(({cat, selected}) => {
+        activeCat = cat
+        console.log(cat)    
+    });
 
     const selectCat = (catId) => {
         navigation.update(nav => ({cat: catId, selected: 0}))
@@ -11,25 +16,34 @@
 </script>
 
 <div id="wrapper">
-    <div class="item" on:click={() => selectCat(cats.EXP.id)}>
+    <div 
+        class="item"
+        class:active={activeCat === cats.EXP.id}
+        on:click={() => selectCat(cats.EXP.id)}>
         <div class="icon">
             <MdBusinessCenter />
         </div>
-        <div><b>EXPERIENCE</b></div>
+        <div><b>{cats.EXP.id}</b></div>
         <div class="counter"><b>4</b></div>
     </div>
-    <div class="item" on:click={() => selectCat(cats.EDU.id)}>
+    <div
+        class="item"
+        class:active={activeCat === cats.EDU.id}
+        on:click={() => selectCat(cats.EDU.id)}>
         <div class="icon">
             <MdSchool />
         </div>
-        <div><b>EDUCATION</b></div>
+        <div><b>{cats.EDU.id}</b></div>
         <div class="counter"><b>3</b></div>
     </div>
-    <div class="item" on:click={() => selectCat(cats.PROJ.id)}>
+    <div
+        class="item"
+        class:active={activeCat === cats.PROJ.id}
+        on:click={() => selectCat(cats.PROJ.id)}>
         <div class="icon">
             <MdFolder />
         </div>
-        <div><b>PROJECTS</b></div>
+        <div><b>{cats.PROJ.id}</b></div>
         <div class="counter"><b>7</b></div>
     </div>
 </div>
@@ -41,7 +55,6 @@
         height: 100%;
         display: flex;
         flex-direction: column;
-        overflow: hidden;
         font-size: 1vw;
     }
     .item {
@@ -52,8 +65,11 @@
         align-items: center;
         color: rgb(170, 168, 168);
     }
-    .item:hover {
-        background-color: #4184B7;
+    .item:hover, .item:hover > .counter {
+        color: white;
+    }
+    .item:not(.active):hover {
+        animation: pulse 1s 1;
     }
     .icon {
         width: 1.2vw;
@@ -64,5 +80,13 @@
         flex-grow: 1;
         text-align: right;
         color: rgb(98, 139, 172);
+    }
+    .item.active {
+        background-color: #4184B7;
+        color: white;
+        transform: scale(1.05);
+    }
+    .item.active > .counter {
+        color: white;
     }
 </style>
