72efa403175ee6e3df53b79443d385dd839cf0eb
devid.farinelli@gmail.com <devid.farinelli@gmail.com>
2019-11-20 14:41:09 +0100

    Fix background and favicon

--- src/App.svelte
+++ src/App.svelte
@@ -5,38 +5,30 @@
 	import Belt from './Belt.svelte'
 	import NavButton from './NavButton.svelte'
 
-	    
-    const appear = (node, { delay = 0, duration = 600}) => ({
+	export let name
+	let introSrc = 'videos/bg-intro.mp4'
+	let bgSrc = 'videos/bg.mp4'
+	let poster = 'https://static.gamespot.com/uploads/original/1406/14063904/3353523-20180211195717_1.jpg'
+	
+	let showProfile
+	let delay = 6000
+	navigation.subscribe((nav) => showProfile = nav.profile )
+
+	let intro
+	let bg
+	const playBg = () => {
+		bg.play();
+		setTimeout(() => {
+			intro.remove()
+			bg.style.opacity = 1
+		}, 100)
+	}
+
+	const appear = (node, { delay = 0, duration = 600}) => ({
 		delay,
 		duration,
 		css: t => `opacity: ${t}`
-	});
-
-	// const blur = (node, { delay = 0, duration = 600, amount = 6}) => ({
-	// 	delay,
-	// 	duration,
-	// 	css: t => 
-	// 		`filter: blur(${t * amount}px);` +
-	// 		`-webkit-filter: blur(${t * amount}px);`
-	// });
-
-	let intro
-	let bg;
-	const playBg = () => {
-		console.log('CIAO', bg, intro)
-		// intro.style.opacity = 0
-		bg.play();
-		setTimeout(() => intro.remove(), 200)
-		// intro.remove()
-	}
-	export let name
-	let introSrc = 'videos/bg-intro.mp4'
-	let bgSrc = 'videos/bg-1-new.mp4'
-	
-	let poster = 'https://static.gamespot.com/uploads/original/1406/14063904/3353523-20180211195717_1.jpg'
-	let showProfile
-	navigation.subscribe((nav) => showProfile = nav.profile )
-	let delay = 6000
+	})
 </script>
 
 <svelte:head>
@@ -46,7 +38,7 @@
 <div class="wrapper">
 	<div id="video-wrapper" class="rateo16-9">
 		<video src={introSrc} {poster} autoplay muted on:ended={playBg}  bind:this={intro}/>
-		<video src={bgSrc} {poster} muted bind:this={bg} />
+		<video id="bg" src={bgSrc} {poster} muted bind:this={bg} />
 	</div>
 	<div id="content" transition:appear={{delay}} class="rateo16-9">
 		<div id="header">
@@ -74,13 +66,13 @@
 	video {
 		width: 100%;
 		height: 100%;
-		/* filter: blur(6px);
-		-webkit-filter: blur(6px); */
 		transform: scale(1.05);
-		background: url('http://api.thumbr.it/whitenoise-361x370.png?background=ffffff00&noise=000000&density=80&opacity=10');
 	}
 	#video-wrapper {
 		overflow: hidden;
+	}
+	#bg {
+		opacity: 0;
 	}
 	.rateo16-9 {
 		width: 100vw;
